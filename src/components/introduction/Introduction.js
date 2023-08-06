492e135a302b7e40e218d4c196b480aae5a5bffb
Tashe Stefacek <tstefacek92@gmail.com>
2023-08-06 14:38:29 -0500

    removes starter React code and adds basic homepage

---
+++ src/components/introduction/Introduction.js
@@ -0,0 +1,12 @@
+import "./Introduction.css";
+
+export default function Introduction() {
+  return (
+    <section id="introduction" role="presentation">
+      <div>
+        <h1>Tashe Stefacek</h1>
+        <h2>a web developer</h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,15 @@
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <a href="">About</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+        </ul>
+      </nav>
+    </>
+  );
+}
