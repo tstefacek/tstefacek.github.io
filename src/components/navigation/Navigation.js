d324c0ce3288ea27c9d3a0e819c7b7d9fbf81f2a
Tashe Stefacek <tstefacek92@gmail.com>
2023-09-12 09:05:13 -0500

    updated resume link

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,7 +6,7 @@ export default function Navigation() {
       <nav>
         <ul>
           <a className="navigation-link" href="#projects-section">Projects</a>
-          <a className="navigation-link" href="https://tinyurl.com/3v53h8ax" target="_blank" rel="noopener noreferrer">Resume</a>
+          <a className="navigation-link" href="https://tinyurl.com/ycfj7fuv" target="_blank" rel="noopener noreferrer">Resume</a>
           <a className="navigation-link" href="#contact-section">Contact</a>
         </ul>
       </nav>
